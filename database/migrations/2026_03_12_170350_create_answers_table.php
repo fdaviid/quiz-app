edb796bb0f6d427616a562a9b8331f9bdfd96014
fdaviid <fldavid02@gmail.com>
2026-03-14 01:00:57 +0200

    constrained questions

--- database/migrations/2026_03_12_170350_create_answers_table.php
+++ database/migrations/2026_03_12_170350_create_answers_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('questionId')->constrained('questions');
+            $table->foreignId('questionId')->constrained('questions')->onDelete('cascade');
             $table->string('answer');
             $table->boolean('isCorrect');
             $table->timestamps();
